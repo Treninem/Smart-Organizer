a3f2a4a22dfa0b45b3bea25d45c292865585ddbe
Treninem <trenin.98@gmail.com>
2026-08-12 23:01:50 -0700

    Fix Windows root literal in folder tree test

--- tests/test_folder_tree.py
+++ tests/test_folder_tree.py
@@ -6,7 +6,7 @@ from core.folder_tree import render_folder_tree
 class FolderTreeTests(unittest.TestCase):
     def test_tree_is_relative_to_last_scan_root(self):
         folders = [
-            {"path": r"D:\Work", "parent": r"D:\", "name": "Work", "depth": 0},
+            {"path": r"D:\Work", "parent": "D:\\", "name": "Work", "depth": 0},
             {"path": r"D:\Work\VoxLyra", "parent": r"D:\Work", "name": "VoxLyra", "depth": 1},
             {"path": r"D:\Work\VoxLyra\Books", "parent": r"D:\Work\VoxLyra", "name": "Books", "depth": 2},
         ]
